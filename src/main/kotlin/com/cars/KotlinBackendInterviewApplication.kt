aaa2df50cd6df0978ce3caac1141347aeb79a180
mikesmith-createfuture <mike.smith@xdesign.com>
2025-08-05 14:32:05 +0100

    Initial Commit'

---
+++ src/main/kotlin/com/cars/KotlinBackendInterviewApplication.kt
@@ -0,0 +1,11 @@
+package com.cars
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBackendInterviewApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinBackendInterviewApplication>(*args)
+}
