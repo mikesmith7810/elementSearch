045f40b1a863469d75406d3ef361985550f0dcc6
mikesmith-createfuture <mike.smith@xdesign.com>
2025-08-21 14:22:59 +0100

    Fixed data file and added filters'

--- src/main/kotlin/com/elements/service/ElementService.kt
+++ src/main/kotlin/com/elements/service/ElementService.kt
@@ -3,11 +3,31 @@ package com.elements.service
 import com.elements.data.DataLoader
 import com.elements.model.Element
 import com.elements.model.ElementSearchRequest
+import com.elements.model.Phase
 import org.springframework.stereotype.Service
 
 @Service
 class ElementService(private val dataLoader: DataLoader) {
     fun searchElements(elementSearchRequest: ElementSearchRequest): List<Element> {
-        return dataLoader.getElements().toList()
+
+        val minDensityFilter: (Element) -> Boolean = { element ->
+            elementSearchRequest.minDensity == null || element.density == "" || element.density.toDouble() >= elementSearchRequest.minDensity
+        }
+        val maxDensityFilter: (Element) -> Boolean = { element ->
+            elementSearchRequest.maxDensity == null || element.density == "" || element.density.toDouble() <= elementSearchRequest.maxDensity
+        }
+        val phaseFilter: (Element) -> Boolean = { element ->
+            elementSearchRequest.phase == Phase.UNKNOWN || element.phase == elementSearchRequest.phase
+        }
+
+        return dataLoader.getElements()
+            .also { elements -> println("Stored elements : ${elements.size}") }
+            .filter(minDensityFilter)
+            .filter(maxDensityFilter)
+            .filter(phaseFilter)
+            .take(elementSearchRequest.limit ?: Int.MAX_VALUE)
+            .also { elements -> println("Filtered elements : ${elements.size}") }
+            .toList()
     }
 }
+
--- src/test/kotlin/com/elements/service/ElementServiceTest.kt
+++ src/test/kotlin/com/elements/service/ElementServiceTest.kt
@@ -10,6 +10,10 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 private const val GAS = "gas"
 private const val NAME_ASC = "name_asc"
@@ -61,5 +65,113 @@ class ElementServiceTest {
 
         elements shouldHaveSize 1
         elements shouldBe storedElements
+    }
+
+    @ParameterizedTest
+    @MethodSource("providesFilteredSearches")
+    fun `should return list when elements match criteria`(
+        elementSearchRequest: ElementSearchRequest, numberOfResults: Int
+    ) {
+        val storedElements = listOf(
+            Element(
+                name = "Hydrogen",
+                symbol = "H",
+                atomicNumber = "1",
+                atomicWeight = "1.008",
+                density = "0.08988",
+                meltingPoint = "-259.16",
+                boilingPoint = "-252.87",
+                phase = Phase.GAS,
+                absoluteMeltingPoint = "13.99",
+                absoluteBoilingPoint = "20.28"
+            ),
+            Element(
+                name = "Carbon",
+                symbol = "C",
+                atomicNumber = "5",
+                atomicWeight = "4.008",
+                density = "6.08988",
+                meltingPoint = "-459.16",
+                boilingPoint = "-252.87",
+                phase = Phase.GAS,
+                absoluteMeltingPoint = "11.99",
+                absoluteBoilingPoint = "25.28"
+            ),
+            Element(
+                name = "Krypton",
+                symbol = "K",
+                atomicNumber = "23",
+                atomicWeight = "4.008",
+                density = "2.08988",
+                meltingPoint = "-269.16",
+                boilingPoint = "-152.87",
+                phase = Phase.SOLID,
+                absoluteMeltingPoint = "16.99",
+                absoluteBoilingPoint = "29.28"
+            )
+        )
+
+        every {
+            dataLoader.getElements()
+        } returns storedElements
+
+        val elements = elementService.searchElements(
+            elementSearchRequest
+        )
+
+        elements shouldHaveSize numberOfResults
+    }
+
+    companion object {
+        @JvmStatic
+        fun providesFilteredSearches(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(
+                    ElementSearchRequest(
+                        minDensity = null,
+                        maxDensity = null,
+                        phase = Phase.UNKNOWN,
+                        sort = null,
+                        limit = null
+                    ), 3
+                ),
+                Arguments.of(
+                    ElementSearchRequest(
+                        minDensity = 5.0,
+                        maxDensity = null,
+                        phase = Phase.UNKNOWN,
+                        sort = null,
+                        limit = null
+                    ), 1
+                ),
+                Arguments.of(
+                    ElementSearchRequest(
+                        minDensity = 1.0,
+                        maxDensity = 8.0,
+                        phase = Phase.UNKNOWN,
+                        sort = null,
+                        limit = null
+                    ), 2
+                ),
+                Arguments.of(
+                    ElementSearchRequest(
+                        minDensity = null,
+                        maxDensity = null,
+                        phase = Phase.GAS,
+                        sort = null,
+                        limit = null
+                    ), 2
+                ),
+                Arguments.of(
+                    ElementSearchRequest(
+                        minDensity = null,
+                        maxDensity = null,
+                        phase = Phase.UNKNOWN,
+                        sort = null,
+                        limit = 1
+                    ), 1
+                )
+            )
+        }
     }
 }
